8f0e26c26904852addb1dfae882768e916995efb
Brandon Knight <knightbrandon1984@gmail.com>
2024-10-25 14:27:58 -0500

    drawing the horizontal segments

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: false,
+    wireframes: true,
     width,
     height,
   },
@@ -114,12 +116,21 @@ const stepThroughCell = (row, column) => {
 
 stepThroughCell(startRow, startColumn);
 
-horizontals.forEach((row) => {
-  row.forEach((open) => {
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
     if (open) {
       return;
     }
 
-    const wall = Bodies.rectangle();
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      {
+        isStatic: true,
+      }
+    );
+    World.add(world, wall);
   });
 });
