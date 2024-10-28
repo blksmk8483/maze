45beaf216c8fc4d69f36980af3012f747fa9e937
Brandon Knight <knightbrandon1984@gmail.com>
2024-10-28 13:26:59 -0500

    determining a win?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 15;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -163,6 +163,7 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
+    label: "goal",
     isStatic: true,
   }
 );
@@ -170,13 +171,13 @@ World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball",
+});
 World.add(world, ball);
 
 document.addEventListener("keydown", (event) => {
   const { x, y } = ball.velocity;
-  console.log(x, y);
-
   if (event.key === "w") {
     Body.setVelocity(ball, { x, y: y - 5 });
   }
@@ -189,4 +190,19 @@ document.addEventListener("keydown", (event) => {
   if (event.key === "a") {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win Condition
+
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console.log("🎉YOU WON!!!!🎉");
+    }
+  });
 });
